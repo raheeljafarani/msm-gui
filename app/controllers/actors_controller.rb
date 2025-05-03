7f43ad9f69b13dd00ed37a9f1367570df344ca1c
raheeljafarani <rjafaran@chicagobooth.edu>
2025-05-03 15:34:22 +0000

    actor edit

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,9 +1,64 @@
 class ActorsController < ApplicationController
+
+  def create
+
+    a = Actor.new
+    a.name = params.fetch("the_name")
+    a.dob = params.fetch("the_dob")
+    a.bio = params.fetch("the_bio")
+    a.image = params.fetch("the_image")
+
+    a.save
+
+    redirect_to("/actors")
+
+    # Retrieve the user's inputs from params
+    # Create a record in the movie table
+    # Populate each column with the user input
+    # Save
+
+    # Redirect the user back to the /movies URL
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Actor.where({ :id => the_id })
+
+    the_actor = matching_records.at(0)
+
+    the_actor.destroy
+
+    redirect_to("/actors")
+  end
+
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
 
     render({ :template => "actor_templates/index" })
+  end
+
+  def update
+    # Get the ID out of params
+    a_id = params.fetch("the_id")
+
+    # Look up the existing record
+    matching_records = Actor.where({ :id => a_id })
+    the_actor = matching_records.at(0)
+
+    # Overwrite each column with the values from user inputs
+    the_actor.name = params.fetch("the_name")
+    the_actor.dob = params.fetch("the_dob")
+    the_actor.bio = params.fetch("the_bio")
+    the_movie.image = params.fetch("the_image")
+
+    # Save
+
+    the_actor.save
+
+    # Redirect to the movie details page
+    redirect_to("/actors/#{the_actor.id}")
   end
 
   def show
